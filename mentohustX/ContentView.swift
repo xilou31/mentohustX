4f51edb45ad49734d2d75ad8096aa179fbd4c6d9
xilou31 <xilou31@qq.com>
2020-10-14 17:23:11 +0800

    增加用户名和密码的UI

--- mentohustX/ContentView.swift
+++ mentohustX/ContentView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import Cocoa
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack{
+            UserName()
+            PassWord()
+        }
     }
 }
 
@@ -18,5 +21,25 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct UserName:View {
+    @State private var userName:String = ""
+    var body: some View{
+        HStack{
+            Text(verbatim: "用户名")
+            TextField("请输入用户名", text: $userName)
+        }
+    }
+}
+
+struct PassWord:View {
+    @State private var passWord:String = ""
+    var body: some View{
+        HStack{
+            Text(verbatim: "密码")
+            SecureField("请输入密码",text:$passWord)
+        }
     }
 }
